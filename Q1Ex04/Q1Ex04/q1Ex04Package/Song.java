bc2ae05eaaed38e33096328b0644aad28172041b
MiaGosh <90023867+MiaGosh@users.noreply.github.com>
2022-10-06 08:10:04 +0800

    Add files via upload

---
+++ Q1Ex04/Q1Ex04/q1Ex04Package/Song.java
@@ -0,0 +1,29 @@
+package q1Ex04Package;
+
+public class Song {
+	private Singer artist;
+	private String title;
+	private String album;
+	int streams=0;
+	
+	public Song(String n, String a) {
+		this.title=n;
+		this.album=a;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public int getStreams() {
+		return streams;
+	}
+	
+	public void setArtist(Singer s) {
+		this.artist=s;
+	}
+	
+	public void viewSong() {
+		System.out.printf("Song %nTitle: %s %nAlbum: %s %nArtist: %s %nStreams: %d %n%n", title, album, artist.getSinger(), streams);
+	}
+}
